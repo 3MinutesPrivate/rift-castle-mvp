5e6deb9c374cfc150c812e997c66f8c5c40fca7a
3MinutesPrivate <chueen.private@gmail.com>
2025-11-29 18:24:25 +0800

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+```ts
+import Phaser from 'phaser';
+import BootScene from './scenes/BootScene';
+import PreloadScene from './scenes/PreloadScene';
+import MainMenuScene from './scenes/MainMenuScene';
+import GameScene from './scenes/GameScene';
+import UIScene from './scenes/UIScene';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'game-container',
+  width: 960,
+  height: 540,
+  backgroundColor: '#1e1e1e',
+  scene: [BootScene, PreloadScene, MainMenuScene, GameScene, UIScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+};
